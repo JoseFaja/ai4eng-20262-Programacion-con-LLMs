9b986a63b9630451a3cbef9c3092b203ad3bd420
Jose Fajardo <josefaja207@gmail.com>
2026-05-24 13:35:15 -0500

    Refactor resumir_actividad_diaria to handle None inputs

--- myanswers/answer-0462.py
+++ myanswers/answer-0462.py
@@ -1,9 +1,21 @@
 import pandas as pd
 
 
-def resumir_actividad_diaria(df, columna_fecha, columna_usuario, columna_evento):
+def resumir_actividad_diaria(
+    df=None,
+    columna_fecha=None,
+    columna_usuario=None,
+    columna_evento=None
+):
+    
+    if df is None:
+        return None
+
     temp = df.copy()
-    temp[columna_fecha] = pd.to_datetime(temp[columna_fecha]).dt.date
+
+    temp[columna_fecha] = pd.to_datetime(
+        temp[columna_fecha]
+    ).dt.date
 
     resumen = temp.groupby(columna_fecha).agg(
         eventos=(columna_evento, "count"),
@@ -11,8 +23,7 @@ def resumir_actividad_diaria(df, columna_fecha, columna_usuario, columna_evento)
     ).reset_index()
 
     umbral = resumen["eventos"].quantile(0.75)
+
     resumen["dia_intenso"] = resumen["eventos"] > umbral
 
     return resumen
-
-
